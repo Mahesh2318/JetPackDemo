ba72ec6bfadb46b9ae5a3c0d529549fe04ff0420
Mahesh Parate <maheshparate23@gmail.com>
2018-10-09 22:31:44 +0530

    first commit

---
+++ app/src/main/java/com/jetpackdemo/ui/start/StartViewModel.kt
@@ -0,0 +1,18 @@
+package com.jetpackdemo.ui.start
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+class StartViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+    private val _data = MutableLiveData<String>()
+
+    val data: LiveData<String>
+        get() = _data
+
+    init {
+        _data.value = "Hello, Jetpack!"
+    }
+
+}
